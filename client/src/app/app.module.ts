eebe27ba495bce317f4acbe40cd12d5478a5840c
jovankabastovanka <jovanaknezevicjovana@gmail.com>
2021-03-01 17:00:26 +0100

    1st March End Of Day

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MatTableModule } from '@angular/material/table';
+import { MemberCardComponent } from './members/member-card/member-card.component'  
 
 @NgModule({
   declarations: [
@@ -33,7 +35,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +44,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    MatTableModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
