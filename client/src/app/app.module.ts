ac7414fe39161476d6871da567f3ec680dc28a44
jovankabastovanka <jovanaknezevicjovana@gmail.com>
2021-03-02 14:06:23 +0100

    End of section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MatTableModule } from '@angular/material/table';
 import { MemberCardComponent } from './members/member-card/member-card.component'  
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     MatTableModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
